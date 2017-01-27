3d7cf04398c1e0e0a4091409570f6f9351e29dbd
Timon Snetselaar <tsnetselaar@xebia.com>
2017-01-27 13:25:39 +0100

    Adds example html and json renderer

---
+++ server/render.go
@@ -0,0 +1,39 @@
+package main
+
+import "net/http"
+import (
+	"encoding/json"
+	"html/template"
+)
+
+const (
+	MIMEJSON = "application/json"
+	MIMEHTML = "text/html"
+)
+
+var templates *template.Template
+
+func JSON(w http.ResponseWriter, obj interface{}) {
+	w.Header().Set("Content-Type", MIMEJSON)
+	json.NewEncoder(w).Encode(obj)
+}
+
+func HTML(w http.ResponseWriter, templateName string, obj interface{}) {
+	w.Header().Set("Content-Type", MIMEHTML)
+	err := templates.ExecuteTemplate(w, templateName, obj)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func initTemplates() *template.Template {
+	templates = template.New("")
+	template.Must(templates.ParseGlob("templates/*/*.html"))
+
+	return templates
+}
+
+func init() {
+	initTemplates()
+}
--- server/routing.go
+++ server/routing.go
@@ -4,11 +4,30 @@ import (
 	"fmt"
 	"golang.org/x/net/context"
 	"net/http"
+	"net/url"
 )
 
 func ExampleHandler(c context.Context, w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "starterkit, you've hit %s\n", r.URL.Path)
 	fmt.Fprintf(w, "context %s\n", c.Value("test"))
+}
+
+func JsonHandler(c context.Context, w http.ResponseWriter, r *http.Request) {
+	JSON(w, &struct {
+		Hello string
+		URLQuery url.Values
+	}{
+		Hello: "test",
+		URLQuery: r.URL.Query(),
+	})
+}
+
+func HtmlHandler(c context.Context, w http.ResponseWriter, r *http.Request) {
+	HTML(w, "index.html", &struct {
+		Title string
+	}{
+		Title: "Exampe Page",
+	})
 }
 
 func AddContext(ctx context.Context, next func(context.Context, http.ResponseWriter, *http.Request)) http.Handler {
@@ -20,7 +39,8 @@ func AddContext(ctx context.Context, next func(context.Context, http.ResponseWri
 func SetupRouting(ctx context.Context) http.Handler {
 	ctx = context.WithValue(ctx, "test", "Test Value")
 	mux := http.NewServeMux()
-	mux.Handle("/", AddContext(ctx, ExampleHandler))
+	mux.Handle("/", AddContext(ctx, HtmlHandler))
+	mux.Handle("/json", AddContext(ctx, JsonHandler))
 
 	return mux
 }
